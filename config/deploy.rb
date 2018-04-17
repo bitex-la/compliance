52305c0a058ac237cd2217aaf122272ecd430859
Yohan Morales <yohanmorales@hotmail.com>
2018-04-17 16:10:28 -0300

    Modify puma log paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,8 @@ set :deploy_to, "/home/ubuntu/apps/#{app_name}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "/var/log/nginx/puma.error.log"
-set :puma_error_log,  "/var/log/nginx/puma.access.log"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/nginx/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
