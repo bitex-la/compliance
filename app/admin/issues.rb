b61bdb919c03bf219df6bd60bfa1376d2e99d96b
nubis <yo@nubis.im>
2018-08-13 12:23:36 -0300

    Add links to issues in dashboard and show meaningful columns

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -7,6 +7,21 @@ ActiveAdmin.register Issue do
   filter :created_at
   filter :updated_at
 
+  index do
+    column(:id)  do |o|
+      link_to o.id, [:admin, :issues, o.id] 
+    end
+    column(:person) do |o|
+      link_to o.person.person_email, [:admin, :people, o.person_id] 
+    end
+    column(:person_enabled)do |o|
+      o.person.enabled
+    end
+    column(:state)
+    column(:created_at)
+    column(:updated_at)
+  end
+
   config.clear_action_items!
   action_item only: [:index] do
     link_to 'New', new_person_issue_path(person)
