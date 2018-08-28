7c93d6797bda1a0c0772b35bf6569e82eb677ab2
Yohan Morales <yohanmorales@hotmail.com>
2018-08-28 13:27:17 -0300

    Add EventLogKind as static model

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -2,16 +2,5 @@ class EventLog < ApplicationRecord
   include StaticModels::BelongsTo
   belongs_to :admin_user, optional: true
   belongs_to :entity, polymorphic: true, optional: true
-  belongs_to :verb, class_name: 'EventLogKind'
-
-=begin  
-  enum verb: %i(
-    create_entity 
-    update_entity 
-    delete_entity
-    harvest_seed
-    observe_entity
-  )
-=end
-  
+  belongs_to :verb, class_name: 'EventLogKind'  
 end
