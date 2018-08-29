984c4bc44725c3932d9c4926443f0b33a7370cfc
Yohan Morales <yohanmorales@hotmail.com>
2018-08-29 10:13:32 -0300

    Add indexes to notes

--- db/migrate/20180828192951_add_indexes_to_note.rb
+++ db/migrate/20180828192951_add_indexes_to_note.rb
@@ -1,4 +1,5 @@
 class AddIndexesToNote < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
   def change
     %i(notes note_seeds).each do |entity|
       add_index entity, :title, :algorithm => :copy
