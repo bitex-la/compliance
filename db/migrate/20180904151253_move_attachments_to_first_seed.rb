1f2106385525f0c744b767b43a34ae1f3cc4d69b
nubis <yo@nubis.im>
2018-09-04 13:37:35 -0300

    Add progress information about seeds sanitizer migration

--- db/migrate/20180904151253_move_attachments_to_first_seed.rb
+++ db/migrate/20180904151253_move_attachments_to_first_seed.rb
@@ -3,9 +3,11 @@ class MoveAttachmentsToFirstSeed < ActiveRecord::Migration[5.1]
     with_identifications = Issue.joins(:identification_seeds)
       .select{|i| i.identification_seeds.size > 1 }
 
-    puts "About to fix #{with_identifications.size} IdentificationSeeds"
-
-    with_identifications.each do |i|
+    all = with_identifications.size
+    i = 0
+    with_identifications.find_each do |i|
+      i += 1
+      puts "Fixing IdentificationSeed #{i}/#{all}"
       i.identification_seeds[1].attachments
         .update_all(attached_to_seed_id: i.identification_seeds[0].id)
     end
@@ -13,9 +15,11 @@ class MoveAttachmentsToFirstSeed < ActiveRecord::Migration[5.1]
     with_allowance = Issue.joins(:allowance_seeds)
       .select{|i| i.allowance_seeds.size > 1 }
 
-    puts "About to fix #{with_allowance.size} AllowanceSeeds"
-
-    with_allowance.each do |i|
+    all = with_allowance.size
+    i = 0
+    with_allowance.find_each do |i|
+      i += 1
+      puts "Fixing Allowance #{i}/#{all}"
       i.allowance_seeds[1].attachments
         .update_all(attached_to_seed_id: i.allowance_seeds[0].id)
     end
@@ -23,9 +27,11 @@ class MoveAttachmentsToFirstSeed < ActiveRecord::Migration[5.1]
     with_domicile = Issue.joins(:domicile_seeds)
       .select{|i| i.domicile_seeds.size > 1 }
 
-    puts "About to fix #{with_domicile.size} DomicileSeeds"
-
-    with_domicile.each do |i|
+    all = with_domicile.size
+    i = 0
+    with_domicile.find_each do |i|
+      i += 1
+      puts "Fixing Domicile #{i}/#{all}"
       i.domicile_seeds[1].attachments
         .update_all(attached_to_seed_id: i.domicile_seeds[0].id)
     end
