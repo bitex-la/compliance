1b663b54adf339bc655480c0b807edd6b3220e61
Yohan Morales <yohanmorales@hotmail.com>
2018-09-25 16:18:53 -0300

    default values for whastapp and telegram

--- app/models/phone_base.rb
+++ app/models/phone_base.rb
@@ -1,6 +1,5 @@
 class PhoneBase < ApplicationRecord
   self.abstract_class = true
-  validates :country, country: true
   validates :phone_kind, inclusion: { in: PhoneKind.all }
   ransackable_static_belongs_to :phone_kind, class_name: "PhoneKind"
 
---
+++ db/migrate/20180925180938_has_social_messaging_as_false_by_default.rb
@@ -0,0 +1,4 @@
+class HasSocialMessagingAsFalseByDefault < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
