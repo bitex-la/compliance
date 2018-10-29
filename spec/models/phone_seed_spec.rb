d1e02778a323e7b2e0f93efa0ccba81a949d0830
Yohan Morales <yohanmorales@hotmail.com>
2018-10-29 14:14:56 -0300

    Fix phone seed spec

--- spec/models/phone_seed_spec.rb
+++ spec/models/phone_seed_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe PhoneSeed, type: :model do
   it { is_expected.to strip_attribute :country }
   it { is_expected.to strip_attribute :note }
 
-  expect(whitespaced_seed.number).to eq '+5491125410470'
-  expect(whitespaced_seed.note).to eq 'please do not call on Sundays'
-  expect(whitespaced_seed.country).to eq 'AR'
-
   it_behaves_like 'whitespaced_seed', described_class.new, {
     number: ' +5491125410470 ',
     phone_kind_code: 'main',
