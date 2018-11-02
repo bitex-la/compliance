ecda4578af41deab2e748ba1ed8979106d6bbb5c
Yohan Morales <yohanmorales@hotmail.com>
2018-11-02 14:45:05 -0300

    Fix failing specs

--- app/models/affinity_seed.rb
+++ app/models/affinity_seed.rb
@@ -2,6 +2,7 @@ class AffinitySeed < AffinityBase
   include Garden::Seed
 
   def linked_once_by_affinity
+    return if issue.nil?
     if affinity_exist?(issue.person, related_person, affinity_kind) || 
       affinity_exist?(related_person, issue.person, affinity_kind)
       errors.add(:base, 'affinity_already_exists')
