9839b1794fb7f5feb754bba18d2c054f17a5486a
Yohan Morales <yohanmorales@hotmail.com>
2018-11-13 14:29:23 -0300

    Add specs for people state transitions

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -61,6 +61,51 @@ RSpec.describe Person, type: :model do
     it 'defaults to new' do 
       expect(empty_person).to have_state(:new)
     end
+
+    %i(unknown new must_reply must_wait all_clear).each do |state|
+      it "goes from #{state} to can_reply on reply_as_enabled" do 
+        expect(person).to transition_from(state).to(:can_reply).on_event(:reply_as_enabled)
+      end
+    end
+
+    %i(unknown new all_clear must_wait).each do |state|
+      it "goes from #{state} to must_reply on reply_as_disabled" do 
+        expect(person).to transition_from(state).to(:must_reply).on_event(:reply_as_disabled)
+      end
+    end
+
+    %i(unknown new all_clear must_reply can_reply).each do |state|
+      it "goes from #{state} to must_wait on wait_for_approval" do 
+        expect(person).to transition_from(state).to(:must_wait).on_event(:wait_for_approval)
+      end
+    end
+
+    %i(unknown new can_reply must_reply must_wait).each do |state|
+      it "goes from #{state} to all_clear on promote" do 
+        expect(person).to transition_from(state).to(:all_clear).on_event(:promote)
+      end
+    end
+
+    it 'goes to must_wait if person is disabled' do 
+      expect(person).to have_state(:all_clear)
+      person.update!(enabled: false)
+
+      expect(person).to have_state(:must_wait)
+
+      person.update(enabled: true)
+      expect(person).to have_state(:all_clear)
+    end
+
+    it 'goes to must_wait if person issue is rejected' do
+      person = create(:empty_person)
+      issue = create(:full_natural_person_issue, person: person)
+      issue.reject!
+
+      expect(person).to have_state(:must_wait)
+
+      person.update(enabled: true)
+      expect(person).to have_state(:all_clear)
+    end
   end
 
   describe 'when calculate status from unknown state' do 
@@ -108,6 +153,19 @@ RSpec.describe Person, type: :model do
       expect(person).to have_state(:must_wait)
     end
 
-    
+    it 'goes from unknown to must reply even if issue state is answered' do 
+      person = create(:empty_person)
+      person.update_column('aasm_state', 'unknown')
+      issue = create(:full_natural_person_issue, person: person)
+      create(:observation, issue: issue)
+      create(:robot_observation, issue: issue)
+      create(:admin_world_check_observation, issue: issue)
+      create(:chainalysis_observation, issue: issue)
+      issue.reload.update_column('aasm_state', 'answered')
+
+      person.reload.sync_status!
+
+      expect(person).to have_state(:must_reply)
+    end
   end
 end
