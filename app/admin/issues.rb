cfd23b68234a5a94644bdd34bfe1501a2ea75184
yohan morales <yohanmorales@hotmail.com>
2018-12-18 12:42:45 -0500

    Refactor Authorization method and action_items rendering

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -55,11 +55,8 @@ ActiveAdmin.register Issue do
 
   Issue.aasm.events.map(&:name).reject{|x| [:observe, :answer].include? x}.each do |action|
     action_item action, only: [:edit, :update], if: lambda { resource.send("may_#{action}?") } do
-      if current_admin_user.is_restricted && !Issue.restricted_actions.include?(action)
-        link_to action.to_s.titleize, [action, :person, :issue], method: :post
-      elsif !current_admin_user.is_restricted
-        link_to action.to_s.titleize, [action, :person, :issue], method: :post
-      end
+      next if Issue.restricted_actions.include?(action) && current_admin_user.is_restricted?
+      link_to action.to_s.titleize, [action, :person, :issue], method: :post
     end
 
     member_action action, method: :post do
--- app/models/admin_access_authorization.rb
+++ app/models/admin_access_authorization.rb
@@ -1,32 +1,30 @@
 class AdminAccessAuthorization < ActiveAdmin::AuthorizationAdapter
+  def allowed_classes
+    [
+      Issue, 
+      Person, 
+      Observation, 
+      NaturalDocketSeed, 
+      LegalEntityDocketSeed,
+      DomicileSeed,
+      IdentificationSeed,
+      AllowanceSeed,
+      AffinitySeed,
+      ArgentinaInvoicingDetailSeed,
+      ChileInvoicingDetailSeed,
+      RiskScoreSeed,
+      PhoneSeed,
+      EmailSeed,
+      FundDeposit,
+      Attachment
+    ]
+  end
+  
+
   def authorized?(action, subject = nil)
-    if user.is_restricted
-      klass = subject.class == Class ? subject : subject.class
-      
-      case action
-      when :read, :create, :update
-        [ Issue, 
-          Person, 
-          Observation, 
-          NaturalDocketSeed, 
-          LegalEntityDocketSeed,
-          DomicileSeed,
-          IdentificationSeed,
-          AllowanceSeed,
-          AffinitySeed,
-          ArgentinaInvoicingDetailSeed,
-          ChileInvoicingDetailSeed,
-          RiskScoreSeed,
-          PhoneSeed,
-          EmailSeed,
-          FundDeposit,
-          Attachment,
-        ].include?(klass)
-      else
-        false
-      end
-    else
-      true
-    end
+    return true unless user.is_restricted?
+
+    klass = subject.class == Class ? subject : subject.class
+    return true if [:read, :create, :update].include?(action) && allowed_classes.include?(klass)
   end
 end
