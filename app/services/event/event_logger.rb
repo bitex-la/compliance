ebbded3c7e22c84a5c623794f2b371823d841b78
Yohan Morales <yohanmorales@hotmail.com>
2019-02-12 11:00:12 -0300

    Refactor event logger entity serialization

--- app/services/event/event_logger.rb
+++ app/services/event/event_logger.rb
@@ -1,12 +1,23 @@
 class Event::EventLogger
   def self.call(entity, user, verb)
     klass = entity.class.name
+
+    serializer = "#{klass}Serializer".constantize
+    relations = serializer.relationships_to_serialize
+    options = {}
+    options[:include] = relations.keys
+    
+    relations.keys.each do |name|
+      naming = Garden::Naming.new(name)
+      naming.serializer.constantize
+    end
+
+    ser = serializer.new(entity, options)
+    body = ser.serialized_json
+
     EventLog.create!(
       entity: entity,
-      raw_data: "#{klass}Serializer".constantize.new(
-        entity,
-        {include: klass.constantize.try(:included_for)}
-      ).serialized_json,
+      raw_data: body,
       admin_user: user,
       verb: verb
     )
