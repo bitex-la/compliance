cab5869981991c697fe2952837fc6d29e85f0a8f
mdallago <mario.dallago@bitex.la>
2019-06-10 16:47:53 -0300

    fixes migration

--- db/migrate/20190605205526_add_reason_to_issues.rb
+++ db/migrate/20190605205526_add_reason_to_issues.rb
@@ -1,7 +1,7 @@
 Dir[Rails.root.join("app/serializers/**/*.rb")].each {|f| require f}
 class AddReasonToIssues < ActiveRecord::Migration[5.2]
   def change
-    add_column :issues, :reason_id, :integer, null:true
+    add_column :issues, :reason_id, :integer
 
     # 1 -> first issue of every person has new_client reason
     Person.all.find_each do |p| 
@@ -12,19 +12,16 @@ class AddReasonToIssues < ActiveRecord::Migration[5.2]
 
     # 5 -> issue with a risk score has new_risk_information reason
     Issue.joins(:risk_score_seeds)
-      .where('reason_id is null').each do |i|
-      i.update_attribute(:reason, IssueReason.new_risk_information) 
-    end
+      .where('reason_id is null')
+      .update_all(reason_id: IssueReason.new_risk_information.id)
 
     # 3 -> issue with defer_until > created_at has update_expired_data reason 
-    Issue.where('reason_id is null and defer_until is not null and defer_until > created_at').each do |i|
-      i.update_attribute(:reason, IssueReason.update_expired_data) 
-    end
+    Issue.where('reason_id is null and defer_until is not null and defer_until > created_at')
+      .update_all(reason_id: IssueReason.update_expired_data.id)
 
     # 2 -> other issues has further_clarification reason
-    Issue.where('reason_id is null').each do |i|
-      i.update_attribute(:reason, IssueReason.further_clarification) 
-    end
+    Issue.where('reason_id is null')
+      .update_all(reason_id: IssueReason.further_clarification.id)
 
     change_column_null :issues, :reason_id, false
   end
