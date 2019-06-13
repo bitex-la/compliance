4546994998f0d2b3719dba8f181892c990e29f20
mdallago <mario.dallago@bitex.la>
2019-06-13 16:53:19 -0300

    fixes tests

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -58,7 +58,35 @@ FactoryBot.define do
       ).each do |name|
         create name, person: person
       end
-      create :full_person_tagging, person: person
+    end
+  end
+
+  factory :full_natural_person_with_tag, class: Person do
+    enabled { true }
+    risk { :medium }
+
+    after(:create) do |person, evaluator|
+      # A full natural person should have at least the issue that created it.
+      # Here we start with a basic issue for this person, then the full
+      # factories inject their seeds into the basic_issue in their after :create
+      create :basic_issue, person: person, aasm_state: 'approved'
+      %i(
+        full_domicile
+        full_risk_score
+        full_natural_person_identification
+        full_natural_docket
+        full_argentina_invoicing_detail
+        full_phone
+        full_email
+        full_note
+        full_affinity
+        full_fund_deposit
+        salary_allowance
+        savings_allowance
+      ).each do |name|
+        create name, person: person
+      end
+      create :full_person_tagging, person: person 
     end
   end
 
