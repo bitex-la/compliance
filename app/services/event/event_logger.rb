b8317b02868cebd2c0a1e48182dd3e5f7c4de997
mdallago <mario.dallago@bitex.la>
2019-06-19 15:00:32 -0300

    revertes event_logger

--- app/services/event/event_logger.rb
+++ app/services/event/event_logger.rb
@@ -3,9 +3,10 @@ class Event::EventLogger
     klass = entity.class.name
     EventLog.create!(
       entity: entity,
-      raw_data: "#{klass}Serializer".constantize.new(	      raw_data: body,
-        entity,	
-        {include: klass.constantize.try(:included_for)}	
+      raw_data: "#{klass}Serializer".constantize.new(
+        raw_data: body,
+        entity,
+        {include: klass.constantize.try(:included_for)}
       ).serialized_json,
       admin_user: user,
       verb: verb
