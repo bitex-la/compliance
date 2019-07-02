2927b753ad4e6febf5c9a8f13c74d9aefc36860d
mdallago <mario.dallago@bitex.la>
2019-07-02 13:19:04 -0300

    adds trigger build on completition

--- .circleci/trigger_build.sh
+++ .circleci/trigger_build.sh
@@ -2,11 +2,11 @@
 
 printf "Triggering an $PROJECT_TO_BUILD build on the $CIRCLE_BRANCH branch\n\n"
 
-if [ "$CIRCLE_BRANCH" != "master" ] 
+if [ "$CIRCLE_BRANCH" == "master" ] 
 then
   BUILD_INFO=$(curl -X POST -H -d \
     "{}" \
-    "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT_TO_BUILD/tree/master?circle-token=$CIRCLE_TOKEN")
+    "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT_TO_BUILD/tree/$CIRCLE_BRANCH?circle-token=$CIRCLE_TOKEN")
 
   printf "\n\nBuild triggered\n\n"
   printf "Follow the progress of the build on \nhttps://circleci.com/gh/$ORGANIZATION/$PROJECT_TO_BUILD/tree/$CIRCLE_BRANCH"
