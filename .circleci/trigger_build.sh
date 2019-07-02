4dc25effceee071297df7cedbb49e965515f2820
mdallago <mario.dallago@bitex.la>
2019-07-02 13:11:35 -0300

    (wip)

--- .circleci/trigger_build.sh
+++ .circleci/trigger_build.sh
@@ -2,7 +2,8 @@
 
 printf "Triggering an $PROJECT_TO_BUILD build on the $CIRCLE_BRANCH branch\n\n"
 
-if [ "$CIRCLE_BRANCH" != "master" ] then
+if [ "$CIRCLE_BRANCH" != "master" ] 
+then
   BUILD_INFO=$(curl -X POST -H -d \
     "{}" \
     "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT_TO_BUILD/tree/$CIRCLE_BRANCH?circle-token=$CIRCLE_TOKEN")
