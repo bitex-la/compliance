2dcbb10d95e965b822b6120eee402a1468695ad9
smtx <seba.rios@gmail.com>
2020-03-03 19:42:07 +0100

    refactor fund_deposit_serializer

--- app/serializers/fund_deposit_serializer.rb
+++ app/serializers/fund_deposit_serializer.rb
@@ -5,12 +5,16 @@ class FundDepositSerializer
   belongs_to :person, record_type: 'people'
   has_many :attachments, record_type: 'attachments'
 
-  attributes *%i(amount currency_code deposit_method_code external_id
-    exchange_rate_adjusted_amount country)
+  attributes *%i(
+    amount
+    currency_code
+    deposit_method_code
+    external_id
+    exchange_rate_adjusted_amount
+    country
+    deposit_date
+    created_at
+    updated_at
+  )
 
-  %i(created_at updated_at deposit_date).each do |attr|
-    attribute attr do |obj|
-      obj.send(attr)
-    end
-  end
 end
