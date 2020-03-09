8c28a0d706050671580c9158b8e71ded81f2aff7
mdallago <mario.dallago@bitex.la>
2020-03-09 15:23:16 -0300

    Add FundDeposits specs

--- app/models/fund_deposit.rb
+++ app/models/fund_deposit.rb
@@ -14,16 +14,26 @@ class FundDeposit < ApplicationRecord
   has_many :attachments, as: :attached_to_fruit
 
   after_save :refresh_person_regularity!
-  after_save{ person.expire_action_cache }
+  after_save { person.expire_action_cache }
+
+  def self.default_scope
+    unless (tags = AdminUser.current_admin_user&.active_tags)
+      return nil
+    end
+
+    return nil if tags.empty?
+
+    where(person_id: Person.all)
+  end
 
   def name
     "##{id}: #{amount} #{currency_code} #{deposit_method_code}"
   end
 
   private
+
   def refresh_person_regularity!
     person.fund_deposits.reload
     person.refresh_person_regularity!
   end
-
 end
