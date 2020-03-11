7b8f8318f860c6dc539569c9177bcd7aa9a84957
smtx <seba.rios@gmail.com>
2020-03-11 20:05:17 +0100

    more specs

--- app/services/risk_assesment/same_person_international_transfers.rb
+++ app/services/risk_assesment/same_person_international_transfers.rb
@@ -4,27 +4,36 @@ module RiskAssesment
       withdrawal_countries = person.fund_withdrawals.pluck(:country)
       deposits_countries = person.fund_deposits.where.not(country: nil).pluck(:country)
 
+      return if deposits_countries.empty? || withdrawal_countries.empty?
+
       unmatched_countries = withdrawal_countries - deposits_countries |
                             deposits_countries - withdrawal_countries
 
       return if unmatched_countries.empty?
 
-      create_issue(person)
+      create_issue(person, unmatched_countries)
     end
 
-    def self.create_issue(person)
+    def self.create_issue(person, unmatched_countries)
       issue = person.issues.build
+
+      fund_withdrawals = person
+                          .fund_withdrawals
+                          .where(country: unmatched_countries)
+      fund_deposits = person
+                        .fund_deposits
+                        .where(country: unmatched_countries)
 
       issue.risk_score_seeds.build(
         replaces: existing_risk_score(person),
         provider: 'open-compliance',
         score: 'same_person_international_transfers',
         extra_info: {
-          fund_withdrawals_count: person.fund_withdrawals.count,
-          fund_withdrawals_sum: person.fund_withdrawals.sum(:exchange_rate_adjusted_amount),
-          fund_deposits_count: person.fund_deposits.count,
-          fund_deposits_sum: person.fund_deposits.sum(:exchange_rate_adjusted_amount)
-        }
+          fund_withdrawals_count: fund_withdrawals.count,
+          fund_withdrawals_sum: fund_withdrawals.sum(:exchange_rate_adjusted_amount),
+          fund_deposits_count: fund_deposits.count,
+          fund_deposits_sum: fund_deposits.sum(:exchange_rate_adjusted_amount)
+        }.to_json
       )
 
       issue.save!
--- spec/services/risk_assesment/same_person_international_transfers_spec.rb
+++ spec/services/risk_assesment/same_person_international_transfers_spec.rb
@@ -19,15 +19,77 @@ describe RiskAssesment::SamePersonInternationalTransfers do
     expect(issue.risk_score_seeds[0].score).to eq 'same_person_international_transfers'
 
     expect(person.risk_scores.count).to eq 1
+    expect(person.risk_scores.first).to eq issue.risk_score_seeds.first.fruit
 
+    extra_info = JSON.parse(person.risk_scores.first.extra_info)
 
-    byebug
-    # validates deposits and withdrawals sum
+    expect(extra_info['fund_withdrawals_count']).to eq person.fund_withdrawals.count
+    expect(extra_info['fund_withdrawals_sum']).to eq person.fund_withdrawals.sum(:exchange_rate_adjusted_amount).to_s
+
+    expect(extra_info['fund_deposits_count']).to eq person.fund_deposits.count
+    expect(extra_info['fund_deposits_sum']).to eq person.fund_deposits.sum(:exchange_rate_adjusted_amount).to_s
   end
 
   it 'does nothing if transfers are from same country' do
+    create(:fund_withdrawal, person: person, country: 'AR')
+    create(:fund_deposit, person: person, country: 'AR')
 
+    RiskAssesment::SamePersonInternationalTransfers.call(person)
+
+    expect(Issue.count).to eq 0
   end
 
-  // escenario en el que hay varios transfer y risk_scores
+  it 'does nothing if there are only deposits' do
+    create(:fund_deposit, person: person, country: 'AR')
+
+    RiskAssesment::SamePersonInternationalTransfers.call(person)
+
+    expect(Issue.count).to eq 0
+  end
+
+  it 'does nothing if there are only withdrawals' do
+    create(:fund_withdrawal, person: person, country: 'AR')
+
+    RiskAssesment::SamePersonInternationalTransfers.call(person)
+
+    expect(Issue.count).to eq 0
+  end
+
+  it 'creates a new approved issue and sum funds correctly' do
+    6.times do
+      create(:fund_withdrawal, person: person, country: 'AR')
+      create(:fund_deposit, person: person, country: 'UY')
+    end
+
+    3.times do
+      create(:fund_withdrawal, person: person, country: 'CL')
+      create(:fund_deposit, person: person, country: 'CL')
+    end
+
+    issue = create(:full_approved_natural_person_issue)
+
+    expect do
+      RiskAssesment::SamePersonInternationalTransfers.call(person)
+    end.to change { Issue.count }.by(1)
+
+    issue = person.reload.issues.last
+
+    expect(issue.approved?).to be true
+    expect(issue.risk_score_seeds.count).to be 1
+    expect(issue.risk_score_seeds[0].provider).to eq 'open-compliance'
+    expect(issue.risk_score_seeds[0].score).to eq 'same_person_international_transfers'
+
+    expect(person.risk_scores.count).to eq 1
+    expect(person.risk_scores.first).to eq issue.risk_score_seeds.first.fruit
+
+    extra_info = JSON.parse(person.risk_scores.first.extra_info)
+    fund_withdrawals = person.fund_withdrawals.where(country: 'AR')
+    fund_deposits = person.fund_deposits.where(country: 'UY')
+
+    expect(extra_info['fund_withdrawals_count']).to eq fund_withdrawals.count
+    expect(extra_info['fund_withdrawals_sum']).to eq fund_withdrawals.sum(:exchange_rate_adjusted_amount).to_s
+
+    expect(extra_info['fund_deposits_count']).to eq fund_deposits.count
+    expect(extra_info['fund_deposits_sum']).to eq fund_deposits.sum(:exchange_rate_adjusted_amount).to_s
+  end
 end
