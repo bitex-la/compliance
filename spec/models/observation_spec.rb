e15e3aca6dece88c0ed5f9be734e44df7012adee
mdallago <mario.dallago@bitex.la>
2020-03-16 18:18:51 -0300

    Add observations model specs

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -123,4 +123,206 @@ RSpec.describe Observation, type: :model do
     issue2.save!
     expect(Observation.history(issue)).to_not include obs2
   end
+
+  describe "When filter by admin tags" do
+    let(:admin_user) { AdminUser.current_admin_user = create(:admin_user) }
+
+    before :each do
+      admin_user.tags.clear
+      admin_user.save!
+    end
+
+    it "allow observation creation only with person valid admin tags" do
+      person1 = create(:full_person_tagging).person
+      person2 = create(:alt_full_person_tagging).person
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      issue1 = create(:basic_issue, person: person1)
+      issue2 = create(:basic_issue, person: person2)
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue1.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+
+      expect { Issue.find(issue2.id) }.to raise_error(ActiveRecord::RecordNotFound)
+
+      admin_user.tags << person2.tags.first
+      admin_user.save!
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue1.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue2.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+    end
+
+    it "allow observation creation with person tags if admin has no tags" do
+      person = create(:full_person_tagging).person
+      issue = create(:basic_issue, person: person)
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+    end
+
+    it "allow observation creation without person tags if admin has no tags" do
+      person = create(:empty_person)
+      issue = create(:basic_issue, person: person)
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+    end
+
+    it "allow observation creation without person tags if admin has tags" do
+      person = create(:full_person_tagging).person
+      issue = create(:basic_issue, person: person)
+
+      admin_user.tags << person.tags.first
+      admin_user.save!
+
+      expect do
+        obs = Observation.new(issue: Issue.find(issue.id))
+        obs.save!
+      end.to change { Observation.count }.by(1)
+    end
+
+    it "Update an obsevation with person tags if admin has tags" do
+      obs1, obs2, obs3, obs4 = setup_for_admin_tags_spec
+      person1 = obs1.issue.person
+      person3 = obs3.issue.person
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      obs = Observation.find(obs1.id)
+      obs.reply = "Some reply here"
+      obs.save!
+
+      obs = Observation.find(obs2.id)
+      obs.reply = "Some reply here"
+      obs.save!
+
+      expect { Observation.find(obs3.id) }.to raise_error(ActiveRecord::RecordNotFound)
+
+      obs = Observation.find(obs4.id)
+      obs.reply = "Some reply here"
+      obs.save!
+
+      admin_user.tags << person3.tags.first
+      admin_user.save!
+
+      obs = Observation.find(obs3.id)
+      obs.reply = "Some reply here"
+      obs.save!
+    end
+
+    it "show observations with admin user active tags" do
+      obs1, obs2, obs3, obs4 = setup_for_admin_tags_spec
+      person1 = obs1.issue.person
+      person3 = obs3.issue.person
+
+      Observation.find(obs1.id)
+      Observation.find(obs2.id)
+      Observation.find(obs3.id)
+      Observation.find(obs4.id)
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      Observation.find(obs1.id)
+      Observation.find(obs2.id)
+      expect { Observation.find(obs3.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      Observation.find(obs4.id)
+
+      admin_user.tags.delete(person1.tags.first)
+      admin_user.tags << person3.tags.first
+      admin_user.save!
+
+      expect { Observation.find(obs1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      Observation.find(obs2.id)
+      Observation.find(obs3.id)
+      Observation.find(obs4.id)
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      Observation.find(obs1.id)
+      Observation.find(obs2.id)
+      Observation.find(obs3.id)
+      Observation.find(obs4.id)
+    end
+
+    it "index observations with admin user active tags" do
+      obs1, obs2, obs3, obs4 = setup_for_admin_tags_spec
+      person1 = obs1.issue.person
+      person3 = obs3.issue.person
+
+      observations = Observation.all
+      expect(observations.count).to eq(4)
+      expect(observations[0].id).to eq(obs1.id)
+      expect(observations[1].id).to eq(obs2.id)
+      expect(observations[2].id).to eq(obs3.id)
+      expect(observations[3].id).to eq(obs4.id)
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      observations = Observation.all
+      expect(observations.count).to eq(3)
+      expect(observations[0].id).to eq(obs1.id)
+      expect(observations[1].id).to eq(obs2.id)
+      expect(observations[2].id).to eq(obs4.id)
+
+      admin_user.tags.delete(person1.tags.first)
+      admin_user.tags << person3.tags.first
+      admin_user.save!
+
+      observations = Observation.all
+      expect(observations.count).to eq(3)
+      expect(observations[0].id).to eq(obs2.id)
+      expect(observations[1].id).to eq(obs3.id)
+      expect(observations[2].id).to eq(obs4.id)
+
+      admin_user.tags << person1.tags.first
+      admin_user.save!
+
+      observations = Observation.all
+      expect(observations.count).to eq(4)
+      expect(observations[0].id).to eq(obs1.id)
+      expect(observations[1].id).to eq(obs2.id)
+      expect(observations[2].id).to eq(obs3.id)
+      expect(observations[3].id).to eq(obs4.id)
+    end
+
+    def setup_for_admin_tags_spec
+      person1 = create(:full_person_tagging).person
+      person2 = create(:empty_person)
+      person3 = create(:alt_full_person_tagging).person
+      person4 = create(:empty_person)
+      person4.tags << person1.tags.first
+      person4.tags << person3.tags.first
+
+      issue1 = create(:basic_issue, person: person1)
+      issue2 = create(:basic_issue, person: person2)
+      issue3 = create(:basic_issue, person: person3)
+      issue4 = create(:basic_issue, person: person4)
+
+      obs1 = create(:robot_observation, issue: issue1)
+      obs2 = create(:robot_observation, issue: issue2)
+      obs3 = create(:robot_observation, issue: issue3)
+      obs4 = create(:robot_observation, issue: issue4)
+
+      [obs1, obs2, obs3, obs4]
+    end
+  end
 end
