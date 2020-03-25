6f1b6c1f262517addd74ce3f057424e72e4016f5
smtx <seba.rios@gmail.com>
2020-03-25 19:53:52 +0100

    Fix migration

--- db/migrate/20200324175440_create_fund_transfers.rb
+++ db/migrate/20200324175440_create_fund_transfers.rb
@@ -1,8 +1,8 @@
 class CreateFundTransfers < ActiveRecord::Migration[5.2]
   def change
     create_table :fund_transfers do |t|
-      t.references :source_person_id, foreign_key: true, null: false
-      t.references :target_person_id, foreign_key: true, null: false
+      t.references :source_person_id, foreign_key: {to_table: :people}, null: false
+      t.references :target_person_id, foreign_key: {to_table: :people}, null: false
       t.decimal :amount, null: false
       t.datetime :transfer_date
       t.decimal :exchange_rate_adjusted_amount,
