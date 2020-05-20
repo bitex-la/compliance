4fd2eed522739af651f8af5156c94e127159b047
Mario <the4power@yahoo.com.ar>
2020-05-20 12:57:10 -0300

    Update spec/helpers/admin_user_helpers.rb
    Co-authored-by: Tomas Rojas <tmsrjs@gmail.com>

--- spec/helpers/admin_user_helpers.rb
+++ spec/helpers/admin_user_helpers.rb
@@ -2,9 +2,9 @@ module AdminUserHelper
   def with_untagged_admin
     old_admin = AdminUser.current_admin_user
     AdminUser.current_admin_user = create(:admin_user)
-    result = yield
+    yield
+  ensure
     AdminUser.current_admin_user = old_admin
-    result
   end
 end
 
