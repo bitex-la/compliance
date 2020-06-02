c297e7286a6949cdbf63f3ff6b59dfd97e030c75
mdallago <mario.dallago@bitex.la>
2020-06-02 10:53:02 -0300

    Add script to update person tags

---
+++ lib/tags_updater.rb
@@ -0,0 +1,16 @@
+class TagsUpdater
+  def self.perform
+    Person.find_each do |person|
+      Rails.logger.info "Processing person #{person.id}"
+      countries = person.fund_deposits.pluck(:country)
+      countries << person.fund_withdrawals.pluck(:country)
+      countries << person.argentina_invoicing_details.try(:country)
+      countries << person.chile_invoicing_details.try(:country)
+      countries << person.domiciles.pluck(:country)
+      countries.flatten.compact.uniq.each do |country|
+        person.refresh_person_country_tagging!(country)
+      end
+    end
+    Rails.logger.info "End of job"
+  end
+end
