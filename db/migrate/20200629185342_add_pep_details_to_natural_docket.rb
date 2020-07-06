a1470744cc0945402bdf6f57824a7cb0c44e378e
werner <werner_a_e@yahoo.es>
2020-07-06 13:02:50 -0300

    Add remote_ip to natural docket

--- db/migrate/20200629185342_add_pep_details_to_natural_docket.rb
+++ db/migrate/20200629185342_add_pep_details_to_natural_docket.rb
@@ -4,9 +4,11 @@ class AddPepDetailsToNaturalDocket < ActiveRecord::Migration[5.2]
     add_column :natural_dockets,      :kind_pep_arg,        :string, null: true
     add_column :natural_dockets,      :full_name_pep_chile, :string, null: true
     add_column :natural_dockets,      :id_pep_chile,        :string, null: true
+    add_column :natural_dockets,      :remote_ip,           :string, null: true
     add_column :natural_docket_seeds, :country_pep,         :string
     add_column :natural_docket_seeds, :kind_pep_arg,        :string, null: true
     add_column :natural_docket_seeds, :full_name_pep_chile, :string, null: true
     add_column :natural_docket_seeds, :id_pep_chile,        :string, null: true
+    add_column :natural_docket_seeds, :remote_ip,           :string, null: true
   end
 end
