1d6b877fd1302d58b2546b97cd1d701e0b3e1fec
mdallago <mario.dallago@bitex.la>
2020-07-24 15:22:16 -0300

    Fix admin user factory

--- spec/factories/admin_user.rb
+++ spec/factories/admin_user.rb
@@ -17,29 +17,18 @@ FactoryBot.define do
     factory :marketing_admin_user do
       admin_role AdminRole.marketing
     end
-  end
 
-  factory :other_admin_user, class: 'AdminUser' do
-    email     { Faker::Internet.email }
-    password  { 'mysecurepassword' }
-    api_token { 'my_other_super_secure_token_for_testing' }
-    admin_role AdminRole.admin
-    max_people_allowed { 1000 }
-  end
+    factory :compliance_admin_user do
+      admin_role AdminRole.compliance
+    end
 
-  factory :compliance_admin_user, class: 'AdminUser' do
-    email     { Faker::Internet.email }
-    password  { 'myrestrictedpassword' }
-    api_token { 'my_restricted_token_for_testing' }
-    admin_role AdminRole.compliance
-    max_people_allowed { 1000 }
-  end
+    factory :other_admin_user do
+      api_token { 'my_other_super_secure_token_for_testing' }
+    end
 
-  factory :limited_people_allowed_admin_user, class: 'AdminUser' do
-    email     { Faker::Internet.email }
-    password  { 'mysecurepassword' }
-    api_token { 'my_super_admin_secure_token_for_testing' }
-    admin_role AdminRole.admin
-    max_people_allowed { 3 }
+    factory :limited_people_allowed_admin_user do
+      api_token { 'my_super_admin_secure_token_for_testing' }
+      max_people_allowed { 3 }
+    end
   end
 end
