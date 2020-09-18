bdf91445ea1d5d4f02ea7197f488a7eeaf2edeee
mdallago <mario.dallago@bitex.la>
2020-09-18 17:13:41 -0300

    Fix permissions

--- app/models/abilities/commercial.rb
+++ app/models/abilities/commercial.rb
@@ -5,8 +5,77 @@ module Abilities
 
     def initialize(user)
       can :read, ActiveAdmin::Page, name: 'Dashboard'
-      can :read, [Issue, Person, Observation]
       can :full_read, Person
+
+      can :read, [Observation,
+        Person,
+        Issue,
+        NaturalDocketSeed,
+        LegalEntityDocketSeed,
+        DomicileSeed,
+        IdentificationSeed,
+        AllowanceSeed,
+        AffinitySeed,
+        ArgentinaInvoicingDetailSeed,
+        ChileInvoicingDetailSeed,
+        RiskScoreSeed,
+        PhoneSeed,
+        EmailSeed,
+        Attachment,
+        NoteSeed,
+        PersonTagging,
+        IssueTagging]
+
+      can :create, [Issue,
+        NaturalDocketSeed,
+        LegalEntityDocketSeed,
+        DomicileSeed,
+        IdentificationSeed,
+        AllowanceSeed,
+        AffinitySeed,
+        ArgentinaInvoicingDetailSeed,
+        ChileInvoicingDetailSeed,
+        RiskScoreSeed,
+        PhoneSeed,
+        EmailSeed,
+        Attachment,
+        NoteSeed,
+        PersonTagging,
+        IssueTagging]
+
+      can :update, [Issue,
+        NaturalDocketSeed,
+        LegalEntityDocketSeed,
+        DomicileSeed,
+        IdentificationSeed,
+        AllowanceSeed,
+        AffinitySeed,
+        ArgentinaInvoicingDetailSeed,
+        ChileInvoicingDetailSeed,
+        RiskScoreSeed,
+        PhoneSeed,
+        EmailSeed,
+        Attachment,
+        NoteSeed,
+        PersonTagging,
+        IssueTagging]
+
+      can :destroy, [NaturalDocketSeed,
+        LegalEntityDocketSeed,
+        DomicileSeed,
+        IdentificationSeed,
+        AllowanceSeed,
+        AffinitySeed,
+        ArgentinaInvoicingDetailSeed,
+        ChileInvoicingDetailSeed,
+        RiskScoreSeed,
+        PhoneSeed,
+        EmailSeed,
+        Attachment,
+        NoteSeed,
+        PersonTagging,
+        IssueTagging]
+
       can :read, user
       can :enable_otp, user
       can :update, user
