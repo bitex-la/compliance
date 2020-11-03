5ed86fbc41f808a1550ada0d8742618e8a03feca
matiasgarcia <garcia.matias92@gmail.com>
2020-11-03 14:30:20 -0300

    Use API v2

--- .circleci/trigger_build.sh
+++ .circleci/trigger_build.sh
@@ -4,9 +4,11 @@ printf "Triggering an $PROJECT_TO_BUILD build on the $CIRCLE_BRANCH branch\n\n"
 
 if [ "$CIRCLE_BRANCH" = "master" ] 
 then
-  BUILD_INFO=$(curl -X POST -H -d \
-    "{}" \
-    "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT_TO_BUILD/tree/$CIRCLE_BRANCH?circle-token=$CIRCLE_TOKEN")
+  BUILD_INFO=$(curl --request POST \
+  --url "https://circleci.com/api/v2/project/github/$ORGANIZATION/$PROJECT_TO_BUILD/pipeline" \
+  --header 'Circle-Token: aae23a3f48ce2b01bcf361f2820411aafb67a7c8' \
+  --header 'content-type: application/json' \
+  --data '{"branch":"master"}')
 
   printf "$BUILD_INFO"
   printf "\n\nBuild triggered\n\n"
