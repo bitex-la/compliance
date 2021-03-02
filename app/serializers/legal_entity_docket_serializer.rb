c9e3eac7130cb7113805f4e79d72d90f4c602149
werner <wechezuria@bitex.la>
2021-03-02 10:36:53 -0300

    Add attributes to serializer

--- app/serializers/legal_entity_docket_serializer.rb
+++ app/serializers/legal_entity_docket_serializer.rb
@@ -1,7 +1,7 @@
 class LegalEntityDocketSerializer
   include FastJsonapiCandy::Fruit
   attributes :industry, :business_description, :country, :commercial_name,
-    :legal_name
+             :legal_name, :regulated_entity, :operations_with_third_party_funds
   build_timestamps
   derive_seed_serializer!
 end
--- spec/factories/legal_entity_docket.rb
+++ spec/factories/legal_entity_docket.rb
@@ -7,6 +7,7 @@ FactoryBot.define_persons_item_and_seed(:legal_entity_docket,
     legal_name { "E Corp" }
     transient{ add_all_attachments { true } }
     regulated_entity { true }
+    operations_with_third_party_funds { false }
   },
   alt_full_legal_entity_docket: proc {
     industry { "agriculture" }
@@ -15,6 +16,7 @@ FactoryBot.define_persons_item_and_seed(:legal_entity_docket,
     commercial_name { "A Corp" }
     legal_name { "A Corp" }
     transient{ add_all_attachments { true } }
+    regulated_entity { true }
     operations_with_third_party_funds { false }
   }
 )
