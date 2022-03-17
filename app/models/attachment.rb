1246c625109f9e49ff47019d22fc23254886646b
matiasgarcia <garcia.matias92@gmail.com>
2022-03-17 11:25:13 -0300

    Fix validation

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -68,7 +68,7 @@ class Attachment < ApplicationRecord
   validates_attachment_size :document,
                             less_than: 2.megabytes,
                             message: lambda {|attachment, metadata| "File #{attachment.document_file_name} size must be lower than 10MB." },
-                            if: :document_changed?
+                            if: lambda { |attachment| attachment.document.dirty? }
 
   def attached_to_something
     return unless attached_to.nil?
