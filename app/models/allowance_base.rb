6af0b0347b4aa56d50e55f4b57367f7d3dba1f2b
Hugo Gallotto <gallottohugo@gmail.com>
2022-05-10 17:54:36 -0300

    Add enum tpi to allowance base

--- app/models/allowance_base.rb
+++ app/models/allowance_base.rb
@@ -5,4 +5,11 @@ class AllowanceBase < ApplicationRecord
   def name_body
     "#{amount} #{kind}"
   end
+
+  enum tpi: %i(_
+               usd_5000_to_10000
+               usd_10000_to_20000
+               usd_20000_to_50000
+               usd_50000_to_100000
+               usd_100000)
 end
