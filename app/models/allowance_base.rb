90bdf8389b453da918a76b28a3b69e7392edb19d
Hugo Gallotto <gallottohugo@gmail.com>
2022-05-11 10:08:26 -0300

    User Person TPI_VALUES constant to enum tpi allowance

--- app/models/allowance_base.rb
+++ app/models/allowance_base.rb
@@ -5,9 +5,5 @@ class AllowanceBase < ApplicationRecord
   def name_body
     "#{amount} #{kind}"
   end
-  enum tpi: { usd_5000_to_10000: 1,
-              usd_10000_to_20000: 2,
-              usd_20000_to_50000: 3,
-              usd_50000_to_100000: 4,
-              usd_100000: 5 }
+  enum tpi: Person::TPI_VALUES
 end
