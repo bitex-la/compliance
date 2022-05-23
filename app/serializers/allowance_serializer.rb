7bdc17d58d48c3f9899a74aac4fa873b6bc06e21
Hugo Gallotto <gallottohugo@gmail.com>
2022-05-23 16:51:09 -0300

    Remove tpi from Allowance Serializer

--- app/serializers/allowance_serializer.rb
+++ app/serializers/allowance_serializer.rb
@@ -1,6 +1,6 @@
 class AllowanceSerializer
   include FastJsonapiCandy::Fruit
-  attributes :weight, :amount, :kind_code, :tpi
+  attributes :weight, :amount, :kind_code
   build_timestamps
   derive_seed_serializer!
 end
