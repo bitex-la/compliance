e5f3c28c3be07f8e8d224a071b00b7612fa7a5bb
Tomás Rojas <tmsrjs@gmail.com>
2022-06-03 13:57:05 -0300

    Add specs for profile history download

--- spec/features/downloadable_files_spec.rb
+++ spec/features/downloadable_files_spec.rb
@@ -22,4 +22,24 @@ describe 'an admin user' do
     DownloadHelpers.wait_for_download
     expect(File.basename(DownloadHelpers.download)).to eq('person_1_kyc_files.zip')
   end
+
+  it 'download user KYC history' do
+    person = create :full_natural_person
+
+    person.should be_enabled
+
+    login_as admin_user
+    click_link('People')
+    click_link('All')
+
+    within("#person_#{person.id} td.col.col-actions") do
+      click_link('View')
+    end
+
+    expect(page.current_path).to eq("/people/#{person.id}")
+
+    open_download_profile_actions_menu('History')
+    DownloadHelpers.wait_for_download
+    expect(File.basename(DownloadHelpers.download)).to eq('person_1_history.zip')
+  end
 end
