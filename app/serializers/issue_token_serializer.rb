64e742cc7bac6dc7d3786c20f887fbf90fa55564
matiasgarcia <garcia.matias92@gmail.com>
2022-08-16 19:50:56 -0300

    Return 200 on correct response

--- app/serializers/issue_token_serializer.rb
+++ app/serializers/issue_token_serializer.rb
@@ -4,5 +4,9 @@ class IssueTokenSerializer
   build_belongs_to :issue
   build_has_many :observations
 
-  attributes :token, :valid_until
+  attributes :token, :valid_until, :expired
+
+  def expired
+    !object.valid_token?
+  end
 end
