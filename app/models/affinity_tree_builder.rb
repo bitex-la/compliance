266b37d949d6009020665b7e2b885f9b64ff74c7
matiasgarcia <garcia.matias92@gmail.com>
2022-09-06 18:58:35 -0300

    Affinity tree builder

--- app/models/affinity_tree_builder.rb
+++ app/models/affinity_tree_builder.rb
@@ -1,30 +1,40 @@
 class AffinityTreeBuilder
-  # In order to avoid an endless loop of affinities, the root_person param is the root of the affinity tree that starts rendering
-  # its affinities. It's used as a mark to avoid rendering more than once that resource.
-  def self.obtain_affinity_tree(root_person, related_person, already_gotten_affinities = [related_person.id, root_person.id])
-    case (related_person_type = related_person.person_type)
+  attr_reader :edges
+
+  def initialize
+    @edges = []
+  end
+
+  def obtain_affinity_tree(parent_person, child_person, already_gotten_affinities = [child_person.id, parent_person.id])
+    add_to_edge(parent_person, child_person)
+
+    case (related_person_type = child_person.person_type)
     when :natural_person
-      [related_person, []]
+      [child_person, []]
     when :legal_entity
       # Not rendering whitelabelers affinities is a requirements definition given that they have thousands of relationships and
       # we don't need to show this information in the affinity tab.
-      return [related_person, []] if related_person.whitelabeler?
+      return [child_person, []] if child_person.whitelabeler?
 
-      legal_entity_affinity_people = related_person
+      legal_entity_affinity_people = child_person
                                        .all_affinities
-                                       .map { |related_person_affinity| related_person_affinity.unscoped_related_one(related_person) }
-                                       .reject { |relevant_person| relevant_person == root_person }
+                                       .map { |related_person_affinity| related_person_affinity.unscoped_related_one(child_person) }
                                        .reject { |relevant_person| relevant_person.id.in?(already_gotten_affinities) }
 
-      new_already_gotten_affinities = [related_person.id].concat(already_gotten_affinities).uniq
+      new_already_gotten_affinities = [child_person.id].concat(already_gotten_affinities).uniq
       [
-        related_person,
-        legal_entity_affinity_people.map do |p|
-          obtain_affinity_tree(root_person, p, new_already_gotten_affinities)
+        child_person,
+        legal_entity_affinity_people.each do |child_of_child|
+          obtain_affinity_tree(child_person, child_of_child, new_already_gotten_affinities)
         end
       ]
     else
       raise "Unknown #{related_person_type}"
     end
+  end
+
+  def add_to_edge(parent_person, child_person)
+    Rails.logger.info("Add edge: #{[parent_person.name, child_person.name]}")
+    edges.push([parent_person.name, child_person.name])
   end
 end
