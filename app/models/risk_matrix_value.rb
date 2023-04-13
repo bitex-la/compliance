0759b2805497fcba25c891258b845b3a85456a57
Hugo Gallotto <gallottohugo@gmail.com>
2023-04-13 14:00:11 -0300

    Remove commets

--- app/models/risk_matrix_value.rb
+++ app/models/risk_matrix_value.rb
@@ -136,9 +136,6 @@ class RiskMatrixValue
   end
 
   def residence_value
-    # TODO: under review
-    # return 0 if person_nationality != country_code and domicile&.country != country_code
-    # return risk_value_nationality if person_nationality == country_code and domicile&.country != country_code
     risk_value_nationality + risk_value_domicile
   end
 
