2e5c2698cd8116fc6d724c8f77f378b762c5755a
Hugo Gallotto <gallottohugo@gmail.com>
2023-05-02 10:05:10 -0300

    Working with note type

--- app/models/note_base.rb
+++ app/models/note_base.rb
@@ -4,8 +4,12 @@ class NoteBase < ApplicationRecord
 
   validates :body, presence: true
   validates :title, length: { maximum: 255 }
+  validates :note_type, presence: true
 
   def name_body
     title || body
   end
+
+  NOTE_TYPE_VALUES = { fiat_note: 1, cripto_note: 2 }.freeze
+  enum note_type: { unknown: 0 }.merge(NOTE_TYPE_VALUES)
 end
