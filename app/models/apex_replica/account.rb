890c56d46cfce6f70b75c4de07f7847867706311
Tomás Rojas <trojas@bitex.la>
2023-05-12 23:46:05 -0300

    Fix Account<->User relationship

--- app/models/apex_replica/account.rb
+++ app/models/apex_replica/account.rb
@@ -5,13 +5,15 @@ module ApexReplica
 
     has_and_belongs_to_many :users, join_table: 'user_account_map'
     has_many :account_configs
+    has_one :user
 
     def self.instance_method_already_implemented?(method_name)
       %w[frozen frozen?].include?(method_name) || super
     end
 
-    def user
-      users.first
+    def bitex_user_id
+      user.user_configs.find_by(config_id: 'bitex_user_id')&.config_value ||
+        account_configs.find_by(config_id: 'bitex_user_id')&.config_value
     end
   end
 end
--- app/models/apex_replica/user.rb
+++ app/models/apex_replica/user.rb
@@ -3,17 +3,25 @@ module ApexReplica
   class User < Base
     has_many :user_configs
     has_and_belongs_to_many :accounts, join_table: 'user_account_map'
-    has_one :default_account, class_name: 'Account', foreign_key: :account_id
+    belongs_to :account
 
     def otp_seed
+      return @seed if @seed
+
       configs = user_configs.to_a
-      type = configs.find { |c| c.config_id == '2FAType' }.config_value
-      seed = configs.find { |c| c.config_id == 'GooglePassPhrase' }.config_value
+      type = configs.find { |c| c.config_id == '2FAType' }&.config_value
+      return unless type.present?
+
+      seed = configs.find { |c| c.config_id == 'GooglePassPhrase' }&.config_value
+      return unless seed.present?
+
       seed = Base32.encode(seed).gsub(/=/, '') if type == 'Google'
-      seed
+      @seed = seed
     end
 
     def totp
+      return unless otp_seed
+
       ROTP::TOTP.new(otp_seed)
     end
 
