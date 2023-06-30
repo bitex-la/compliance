e920280490c265ba3b53d6453ad2e252c8183c56
Hugo Gallotto <gallottohugo@gmail.com>
2023-06-30 11:27:47 -0300

    Working on filter movements and notes by specific date and auditor email

--- app/models/note_seed.rb
+++ app/models/note_seed.rb
@@ -1,3 +1,7 @@
 class NoteSeed < NoteBase
   include Garden::Seed
+
+  def self.note_seeds_condition
+    where('note_seeds.created_at > ?', DateTime.parse(Settings.fiat_only.start_date)) if AdminUser.current_admin_user&.fiat_only?
+  end
 end
